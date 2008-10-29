0ec05e2ab5c1daa76e3003abc6a872f2a6c98ab2
Thor Harald Johansen <thor@thor-pc.(none)>
2008-10-29 23:01:09 +0100

    Foo

--- src/modules.cpp
+++ src/modules.cpp
@@ -177,6 +177,12 @@ parameter_properties multichorus_audio_module::param_props[] = {
 
 synth::ladspa_plugin_info multichorus_audio_module::plugin_info = { 0x8501, "MultiChorus", "Calf MultiChorus", "Krzysztof Foltman", synth::calf_copyright_info, "ChorusPlugin" };
 
+parameter_properties compressor_audio_module::param_props[] = {
+    { 1,      0, 1,    0, PF_FLOAT | PF_SCALE_LOG | PF_CTL_KNOB | PF_UNIT_DB, NULL, "threshold", "Threshold" }
+};
+
+synth::ladspa_plugin_info compressor_audio_module::plugin_info = { 0x8502, "Compressor", "Calf Compressor", "Thor Harald Johansen", synth::calf_copyright_info, "CompressorPlugin" };
+
 ////////////////////////////////////////////////////////////////////////////
 
 template<class Module>
