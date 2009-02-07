f6c936aac17c7656011bfe8e567af90cdb2c4a51
Krzysztof Foltman <wdev@foltman.com>
2009-02-07 21:54:01 +0000

    + Monosynth: add pitch bend range knob

--- src/modules.cpp
+++ src/modules.cpp
@@ -285,6 +285,8 @@ CALF_PORT_PROPS(monosynth) = {
 
     { 0.5,         0,   1, 100, PF_FLOAT | PF_SCALE_GAIN | PF_CTL_KNOB | PF_PROP_OUTPUT_GAIN, NULL, "master", "Volume" },
 
+    { 200,     -2400, 2400,   49, PF_FLOAT | PF_SCALE_LINEAR | PF_CTL_KNOB | PF_UNIT_CENTS, NULL, "pwhl_range", "PWheel Range" },
+
     { 5,       0.01, 20,    0, PF_FLOAT | PF_SCALE_LOG | PF_CTL_KNOB | PF_UNIT_HZ, NULL, "lfo_rate", "LFO Rate" },
     { 0.5,      0.1,  5,    0, PF_FLOAT | PF_SCALE_LOG | PF_CTL_KNOB | PF_UNIT_SEC, NULL, "lfo_delay", "LFO Delay" },
     { 0,      -4800, 4800,  0, PF_FLOAT | PF_SCALE_LINEAR | PF_CTL_KNOB | PF_UNIT_CENTS, NULL, "lfo2filter", "LFO->Filter" },
