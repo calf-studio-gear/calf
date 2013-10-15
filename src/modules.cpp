bbbe76090076eb6946e4d2d65bf1bc11910a488e
Markus Schmidt <schmidt@boomshop.net>
2013-10-16 01:23:27 +0200

    value changes

--- src/modules.cpp
+++ src/modules.cpp
@@ -2057,7 +2057,7 @@ uint32_t transientdesigner_audio_module::process(uint32_t offset, uint32_t numsa
             
             // amplification factor from attack and release curve
             float ampfactor = attdiff
-                          * (*params[param_attack_boost] > 0 ? *params[param_attack_boost] * 4 : *params[param_attack_boost] * 2)
+                          * (*params[param_attack_boost] > 0 ? *params[param_attack_boost] * 4 : *params[param_attack_boost] * 4)
                           + (*params[param_release_boost] > 0 ? *params[param_release_boost] * 2 : *params[param_release_boost] * 8)
                           * ((*params[param_release_boost] > 0) ? ((release - reldiff == 0) ? 0 : (release / (release - reldiff) - 1)) : reldiff);
             
